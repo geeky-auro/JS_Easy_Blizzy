27611110f9e7991e5cdae1b7ecce30ddf3b35f1e
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-04 22:27:00 +0530

    modified

---
+++ yourPlayground.js
@@ -0,0 +1,22 @@
+console.log('hello world')
+console.log('Hello Auro Saswat ')
+
+// Variables
+name='Auro'
+console.log(name)
+sentance ='How are you doing today,nice to see, hope you have a good day!'
+
+// Operators
+food=prompt("How much was the food")
+tipPercentage=prompt("How much Tip would you like to give")/100
+tipAmount=food*tipPercentage
+total=food+tipAmount
+// '100'+'20'
+console.log(total)
+console.log(tipAmount)
+// instead of console we can use it in alert()
+// alert(tipAmount)
+
+// User Input
+// fruit=prompt('What is your Favourite food')
+// console.log(fruit)
