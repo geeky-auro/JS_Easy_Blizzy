b835c389402770faf0b9a2e337e1ab1ac17fe36c
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-08-04 23:02:02 +0530

    Baby App Updated

--- yourPlayground.js
+++ yourPlayground.js
@@ -6,17 +6,66 @@ name='Auro'
 console.log(name)
 sentance ='How are you doing today,nice to see, hope you have a good day!'
 
+
+/* There are 3 types of variables
+
+🔹var ---> Outdated
+🔹let ---> Acts as a Non-Final Type 🍾
+🔹const --> Acts as a Final Type
+
+*/
+
+// Baby Weather App (conditionals)
+// if rain 👉🏼 "Grab your Umbrella ☔"
+// else 👉🏼"Wearyour Sunglasses 😎"
+// Use Twillo For Real time SMS Messaging..!
+
+let weather=prompt("How is the weather")
+if(weather=='rainy'){
+  console.log('Grab your Umbrella ☔')
+}
+else{
+  console.log('Wearyour Sunglasses 😎')
+}
+
+
+
+
 // Operators
-food=prompt("How much was the food")
-tipPercentage=prompt("How much Tip would you like to give")/100
+/*food=Number(prompt("How much was the food"))
+tipPercentage=Number(prompt("How much Tip would you like to give"))/100
 tipAmount=food*tipPercentage
 total=food+tipAmount
 // '100'+'20'
-console.log(total)
-console.log(tipAmount)
+console.log("Total",total)
+console.log("Tip Amount",tipAmount)
 // instead of console we can use it in alert()
 // alert(tipAmount)
-
+*/
 // User Input
 // fruit=prompt('What is your Favourite food')
 // console.log(fruit)
+
+/* Data Types 
+numbers 👉🏼 1,5,10,100,2.5
+strings 👉🏼 'hello',"What is up"
+objects 👉🏼 {}
+arrays 👉🏼 []
+boolean 👉🏼 true / false
+*/
+
+/* Maths Operations
+👉🏼Multiply-> *
+👉🏼Divide-> /
+👉🏼Exponents-> **
+👉🏼Add-> +
+👉🏼Subtract-> -
+👉🏼Modulus/Reminders-> %
+*/
+
+/* Math Operations
+Floor - Rounds down--> Math.floor()
+Ceil - Rounds up--> Math.ceil()
+Random - gives u a Random Number between 0 AND 1--> Math.random()
+*/
+
